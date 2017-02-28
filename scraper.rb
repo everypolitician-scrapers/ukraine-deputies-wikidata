99fd400f08f3efca2f20bbbc093108c98959c974
Tony Bowden <tony@tmtm.com>
2017-02-28 08:54:07 +0000

    Use SPARQL to get Wikidata members

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,15 @@
 require 'pry'
 require 'wikidata/fetcher'
 
+sparq = <<EOS
+  SELECT ?item
+  WHERE {
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 wd:Q12132454 ;    # People's Deputy of Ukraine
+                        pq:P2937 wd:Q18562210 .  #   parliamentary term: 8th
+  }
+EOS
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/ukraine-deputies-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { uk: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { uk: names })
